62edcc1e58595e8a77e4b550e42523de60fd60dc
Arka <arka.spectacular@gmail.com>
2021-07-21 15:23:39 +0530

    [Arka]Add:Calculate Length of Line

---
+++ Line_Comparison_Assignment/Constructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Constructor
+{
+	public int pointer1, pointer2, pointer3,pointer4;
+	public Constructor(int x ,int y, int z, int w)
+	{
+		this.pointer1 = x;
+		this.pointer2 = y;
+		this.pointer3 = z;
+		this.pointer4 = w;
+
+
+	}
+	
+}
