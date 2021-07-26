8ec6d485b583963151b7526c5b0abb7200e5cc05
Arka <arka.spectacular@gmail.com>
2021-07-26 11:53:34 +0530

    [Arka]Refactor:Code refacted using .Equals and .CompareTo keywords

--- Line_Comparison_Assignment/Program.cs
+++ Line_Comparison_Assignment/Program.cs
@@ -10,28 +10,25 @@ namespace Line_Comparison_Assignment
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem!");
-            Line obj1 = new Line(9, 17, 7, 12);
+            Line obj1 = new Line(1, 1, 1, 1);
             double length_Of_line1 = obj1.Line_Length();
-            Line obj2 = new Line(8, 15, 6, 9);
+            Line obj2 = new Line(1, 1, 1, 1);
             double length_Of_line2 = obj2.Line_Length();
             Console.WriteLine("Length of Line 1 is :" + length_Of_line1);
             Console.WriteLine("Length of Line 2 is :" + length_Of_line2);
-            if (length_Of_line1 > length_Of_line2)
+            int compare = length_Of_line1.CompareTo(length_Of_line2);
+            if (compare >0)
             {
                 Console.WriteLine("Length of Line 1 is grater than Line 2 ");
             }
-            if (length_Of_line1 == length_Of_line2)
+            else if(compare <0 )
+            {
+                Console.WriteLine("Length of Line 2 is grater than line 1");
+            }
+            if (length_Of_line1.Equals(length_Of_line2) | compare == 0)   //just to show .Equals keyword
             {
                 Console.WriteLine("Length of Line 1 equal to Line 2 ");
             }
-            else
-            {
-                Console.WriteLine("Length of Line 2 is grater than Line 1 ");
-            }
-
-
-
-
         }
 
     }
